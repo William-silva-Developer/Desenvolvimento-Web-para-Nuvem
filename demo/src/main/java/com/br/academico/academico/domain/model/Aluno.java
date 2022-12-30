81929ea48e51faa5168ed582ad170139eef77db8
EC2 Default User <ec2-user@ip-172-31-8-82.ec2.internal>
2022-12-30 14:32:11 +0000

    criando o model

---
+++ demo/src/main/java/com/br/academico/academico/domain/model/Aluno.java
@@ -0,0 +1,6 @@
+package com.br.academico.academico.domain.model;
+
+public class Aluno {
+    
+    
+}
---
+++ demo/src/main/java/com/br/academico/academico/domain/model/Professor.java
@@ -0,0 +1,5 @@
+package com.br.academico.academico.domain.model;
+
+public class Professor {
+    
+}
