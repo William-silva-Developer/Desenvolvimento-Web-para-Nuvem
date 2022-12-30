9952c546be4e75b3a6faf46fc34b898275e29fbc
EC2 Default User <ec2-user@ip-172-31-8-82.ec2.internal>
2022-12-30 14:27:26 +0000

    criacao dos modelos aluno e professor

--- demo/src/main/java/com/br/academico/academico/DemoApplication.java
+++ demo/src/main/java/com/br/academico/academico/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.br.academico.demo;
+package com.br.academico.academico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
