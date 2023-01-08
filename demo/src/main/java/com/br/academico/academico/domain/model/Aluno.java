e8457a268be7eb243efc87edcdb8f51a291cadeb
William-silva-Developer <98723501+William-silva-Developer@users.noreply.github.com>
2023-01-07 23:14:42 -0300

    Update Aluno.java
    Criando o model Aluno

--- demo/src/main/java/com/br/academico/academico/domain/model/Aluno.java
+++ demo/src/main/java/com/br/academico/academico/domain/model/Aluno.java
@@ -1,6 +1,45 @@
 package com.br.academico.academico.domain.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "aluno")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Aluno {
-    
-    
+	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
+	private Long id;
+	
+	@NotBlank
+	private String nome;
+	
+	@NotBlank
+	private String matricula;
+	
+	@NotBlank
+	@Email(message = "Este email não é válido")
+	private String email;
+
+	@NotBlank
+	private String senha;
+	
+
 }
