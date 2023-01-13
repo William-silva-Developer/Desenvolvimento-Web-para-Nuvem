893d3bba317bfd9afe4a8865ec2baf71173b18d2
Amanda Rosa <amandarcrosa@yahoo.com>
2023-01-13 13:28:47 +0000

    feature: Criação do model Frequência Diário

---
+++ demo/src/main/java/com/br/academico/academico/domain/model/FrequenciaDiario.java
@@ -0,0 +1,40 @@
+package com.br.academico.academico.domain.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "frequenciaDiario")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class FrequenciaDiario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
+	private Long id;
+
+    @NotBlank
+    private String matriculaAluno;
+    
+    @NotBlank
+    private int numeroFaltas;
+
+    @NotBlank
+    private Date date;
+    
+}
