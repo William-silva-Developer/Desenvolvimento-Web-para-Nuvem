092f661981d0595c56e8abb28f9295bbcf200a20
William-sWilva-Developer <william958silva@gmail.com>
2023-01-17 14:28:06 +0000

    Novas configuracoes

--- demo/src/main/java/com/br/academico/academico/DemoApplication.java
+++ demo/src/main/java/com/br/academico/academico/DemoApplication.java
@@ -1,9 +1,22 @@
 package com.br.academico.academico;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@EnableJpaRepositories(basePackages = {"com.br.academico.academico.domain.repository"})
+@EntityScan(basePackages = {"com.br.academico.academico.domain.model"})
+@EnableTransactionManagement
+@EnableWebMvc
+@RestController
+@EnableAutoConfiguration
 public class DemoApplication {
 
 	public static void main(String[] args) {
